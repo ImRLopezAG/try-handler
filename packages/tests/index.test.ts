cf2879a465990e2c5985d6efd8b7d775dd7a5c84
Angel Gabriel Lopez Solano <alopez@dextra.com.do>
2024-08-19 12:09:05 -0400

    refactor: Update tryAsync and trySync functions to consistently return error-first signature

--- packages/tests/index.test.ts
+++ packages/tests/index.test.ts
@@ -1,79 +1,91 @@
-import assert from 'node:assert';
-import { describe, it } from 'node:test';
-import { tryAsync, trySync } from 'try-handler';
+import assert from 'node:assert'
+import { describe, it } from 'node:test'
+import { tryAsync, trySync } from 'try-handler'
+
+interface Todo {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
 
 describe('tryAsync', () => {
   it('should return data and null when the function is successful', async () => {
     const [error, data] = await tryAsync(async () =>
       Promise.resolve('Hello, world!')
-    );
-    assert.strictEqual(data, 'Hello, world!');
-    assert.strictEqual(error, null);
-  });
+    )
+    assert.strictEqual(data, 'Hello, world!')
+    assert.strictEqual(error, null)
+  })
 
   it('should return null and error message when the function throws an error', async () => {
     const [error, data] = await tryAsync(async () => {
-      throw new Error('Something went wrong!');
-    });
-    assert.strictEqual(data, null);
-    assert.strictEqual(error, 'Something went wrong!');
-  });
+      throw new Error('Something went wrong!')
+    })
+    assert.strictEqual(data, null)
+    assert.strictEqual(error, 'Something went wrong!')
+  })
 
   it('should return data and null when the function is successful', async () => {
-    const [error, data] = trySync(() => 'Hello, world!');
-    assert.strictEqual(data, 'Hello, world!');
-    assert.strictEqual(error, null);
-  });
+    const [error, data] = trySync(() => 'Hello, world!')
+    assert.strictEqual(data, 'Hello, world!')
+    assert.strictEqual(error, null)
+  })
 
   it('should return null and error message when the function throws an error', async () => {
     const [error, data] = trySync(() => {
-      throw new Error('Something went wrong!');
-    });
-    assert.strictEqual(data, null);
-    assert.strictEqual(error, 'Something went wrong!');
-  });
+      throw new Error('Something went wrong!')
+    })
+    assert.strictEqual(data, null)
+    assert.strictEqual(error, 'Something went wrong!')
+  })
 
   it('should return null and error message when the function throws an error', async () => {
     const [error, data] = trySync(() => {
-      throw 'Something went wrong!';
-    });
-    assert.strictEqual(data, null);
-    assert.strictEqual(error, 'Something went wrong!');
-  });
+      throw 'Something went wrong!'
+    })
+    assert.strictEqual(data, null)
+    assert.strictEqual(error, 'Something went wrong!')
+  })
 
   it('should return null and error message when the function throws an error', async () => {
     const [error, data] = trySync(() => {
-      throw 404;
-    });
-    assert.strictEqual(data, null);
-    assert.strictEqual(error, '404');
-  });
+      throw 404
+    })
+    assert.strictEqual(data, null)
+    assert.strictEqual(error, '404')
+  })
 
   it('should return null and error message when the function throws an error', async () => {
     const [error, data] = trySync(() => {
-      throw null;
-    });
-    assert.strictEqual(data, null);
-    assert.strictEqual(error, 'null');
-  });
+      throw null
+    })
+    assert.strictEqual(data, null)
+    assert.strictEqual(error, 'null')
+  })
 
   it('should return TODOS and null when the function is successful calling an API', async () => {
-    const [error, data] = await tryAsync<
-      {
-        userId: number;
-        id: number;
-        title: string;
-        completed: boolean;
-      }[]
-    >(async () => {
+    const [error, data] = await tryAsync<Todo[]>(async () => {
       const response = await fetch(
         'https://jsonplaceholder.typicode.com/todos'
-      );
-      return response.json();
-    });
-    assert.strictEqual(error, null);
-    if (data) {
-      assert.strictEqual(data.length, 200);
-    }
-  });
-});
+      )
+      return await response.json()
+    })
+    assert.strictEqual(error, null)
+    assert.strictEqual(data?.length, 200)
+  })
+
+  it('should return null and error message when the function throws an error calling an API', async () => {
+    const [error, data] = await tryAsync<Todo>(async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/todos/0'
+      )
+      if (!response.ok) {
+        throw response.statusText
+      }
+      return await response.json()
+    })
+    assert.strictEqual(data, null)
+    assert.strictEqual(error, 'Not Found')
+  })
+})

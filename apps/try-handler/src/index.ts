62321b269d05dc3592b1357c1af811ab56d9a25c
Angel Gabriel Lopez TSX <angel178293lopez@gmail.com>
2024-09-13 08:18:33 -0400

    Add oneof type

--- apps/try-handler/src/index.ts
+++ apps/try-handler/src/index.ts
@@ -2,6 +2,23 @@
 type TryResult<T> = [ErrorHandling | null, T | undefined];
 type Callback<T> = () => T;
 
+type MergeTypes<
+  TypesArray extends any[],
+  Res = Record<string, never>,
+> = TypesArray extends [infer Head, ...infer Rem]
+  ? MergeTypes<Rem, Res & Head>
+  : Res;
+
+type OneOf<
+  TypesArray extends any[],
+  Res = never,
+  AllProperties = MergeTypes<TypesArray>,
+> = TypesArray extends [infer Head, ...infer Rem]
+  ? OneOf<Rem, Res | OnlyFirst<Head, AllProperties>, AllProperties>
+  : Res;
+
+type OnlyFirst<F, S> = F & { [Key in keyof Omit<S, keyof F>]?: never };
+
 interface ErrorHandling {
   message: string;
   instance: {
